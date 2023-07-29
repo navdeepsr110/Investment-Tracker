6ab8ce7d0773c52d6bcc2426436a4b8ddec8da94
Navdeep <rathorenavdeep110@gmail.com>
2023-07-29 19:01:57 +0530

    Investment Calculator Completed

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import logo from "../../Assets/investment.png";
+import classes from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+    );
+};
+
+export default Header;
